819159c2b03822fa024c0894ac51c15197722b4f
Zahrotunnn <cantiknafisa269@gmail.com>
2024-06-01 12:46:02 +0700

    Add files via upload

---
+++ game 1.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="img/flowers.png" type="image/x-icon">
+    <title>Flowers</title>
+</head>
+
+  <body>
+  <div class="greetings">
+  <span>H</span>
+  <span>A</span>
+  <span>L</span>
+  <span>O</span>
+  <span>A</span>
+  <span>B</span>
+  <span>I</span>
+  <span>D</span>
+  <span>Z</span>
+  <span>A</span>
+  <span>R</span>
+  <span>!</span>
+  </div>
+  <div class="description">
+  <span>flowers for youu</span>
+  
+ </div>
+  <div class="button">
+  <button>
+  <a href="flower.html">ketuk disini
+  </a>
+  </button>
+  
+  
+  </div>
+  </body>
+  
+  </html>
+  
